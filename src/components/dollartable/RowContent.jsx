dca65897b1c397d07da7cde3e5a32a053cfe821d
Oscar Vargas Z <ovargas@gmail.com>
2024-06-17 02:21:36 -0400

    chore: Remove edit and delete buttons from RowContent component

--- src/components/dollartable/RowContent.jsx
+++ src/components/dollartable/RowContent.jsx
@@ -1,40 +1,14 @@
-import { TableCell, IconButton } from '@mui/material'
-import EditIcon from '@mui/icons-material/Edit'
-import DeleteIcon from '@mui/icons-material/Delete'
+import { TableCell } from '@mui/material'
 import { columns } from './logic/dataLoad'
 
 export default function RowContent (_index, row) {
-  const handleEdit = (id) => {
-    console.log('Editar:', id)
-  }
-
-  const handleDelete = (id) => {
-    console.log('Eliminar:', id)
-  }
-
   return (
     <>
-      {columns.map((column, columnIndex) => (
-        <TableCell key={`${row.id}-${column.dataKey}`}>
+      {columns.map((column) => (
+        <TableCell
+          key={`${row[column.dataKey]}-${row.id}`}
+        >
           {row[column.dataKey]}
-          {columnIndex === columns.length - 1 && (
-            <>
-              <IconButton
-                id={`editButton${row.id}`}
-                color='primary'
-                onClick={() => handleEdit(row.id)}
-              >
-                <EditIcon />
-              </IconButton>
-              <IconButton
-                id={`deleteButton${row.id}`}
-                color='secondary'
-                onClick={() => handleDelete(row.id)}
-              >
-                <DeleteIcon />
-              </IconButton>
-            </>
-          )}
         </TableCell>
       ))}
     </>
